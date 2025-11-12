591add6fe2042d4bb9b430b0d9140b30b46538ed
Harsh Kandpal <harshksndpal1384@gmail.com>
2025-11-12 12:14:00 +0530

    time complexities

--- MergeSort.java
+++ MergeSort.java
@@ -1,21 +1,24 @@
 import java.util.*;
 
 public class MergeSort{
+    //nlogn
     public static void divide(int arr[], int si, int ei){
         if(si >= ei){
             return;
         }
+        //O(logn)
         int mid = si + (ei - si) / 2; // si + ei / 2 may cause overflow
         divide(arr, si, mid);
         divide(arr, mid + 1, ei);
         conquer(arr, si, mid, ei);
     }
+    
     public static void conquer(int arr[], int si, int mid, int ei){
         int merged[] = new int[ei - si + 1];
         int idx1 = si;
         int idx2 = mid + 1;
         int x = 0;
-
+        //O(n)
         while(idx1 <= mid && idx2 <= ei){
             if(arr[idx1] <= arr[idx2]){
                 merged[x++] = arr[idx1++]; // post increment or you can write in nexr line x++; idx1++;
